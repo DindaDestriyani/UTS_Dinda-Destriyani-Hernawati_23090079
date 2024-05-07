5dcfa76fbcae032cb95e7cec6137a550102d3434
DindaDestriyani <145641627+DindaDestriyani@users.noreply.github.com>
2024-05-07 17:11:09 +0700

    Add files via upload

--- UTS_Dinda Destriyani Hernawati_23090079/soal_1.py
+++ UTS_Dinda Destriyani Hernawati_23090079/soal_1.py
@@ -1,12 +1,15 @@
-flowchart
-Start
-|-> Input Bilangan Pertama
-  |-> Decision: a + b
-|    |-> Flow: hitung hasil penjumlahan
-  |    |    |-> Data: 5 + 20
-  |    |-> End
-  |-> Decision: a - b
-|    |-> Flow: hitung hasil pengurangan
-  |    |    |-> Data: 5 - 20
-  |    |-> End
-End
+def penjumlahan(a,b):
+    return a + b
+def pengurangan(a,b):
+    return a - b
+def modulus(a,b):
+    return a % b
+
+bilangan1 = int(input('Masukkan bilangan pertama : '))
+bilangan2 = int(input('Masukkan bilangan kedua : '))
+
+print('\n')
+
+print('Hasil penjumlahan dari bilangan tersebut ', penjumlahan(bilangan1,bilangan2))
+print('Hasil pengurangan dari bilangan tersebut ', pengurangan(bilangan1,bilangan2))
+print('Hasil modulus dari bilangan tersebut ', modulus(bilangan1,bilangan2))
